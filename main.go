a7c487cf64981d056002b4dce9316ffdc1e0412b
Mason Johnson <mason@invariant.dev>
2020-08-03 12:42:47 -0600

    Refactored to support multiple image uploads

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"archive/tar"
 	"bytes"
 	"fmt"
 	"image"
@@ -22,38 +23,93 @@ func main() {
 }
 
 func upload(w http.ResponseWriter, r *http.Request) {
-	r.ParseMultipartForm(100 << 20)
+	// Parse incoming form, allow 100MB in memory
+	err := r.ParseMultipartForm(100 << 20)
+	if err != nil {
+		w.Write([]byte(fmt.Sprintf("Error parsing form: %v", err)))
+	}
+
+	formData := r.MultipartForm
+	images := formData.File["image"]
+
 	format := r.FormValue("format")
 	switch format {
 	case "png", "jpg", "jpeg", "gif":
-		fmt.Println("converting to", format)
-		f, fheader, err := r.FormFile("image")
-		if err != nil {
-			w.Write([]byte(fmt.Sprintf("Error uploading file: %v", err)))
-			return
-		}
-		convImg, err := convert(format, f)
-		if err != nil {
-			w.Write([]byte(fmt.Sprintf("Error converting file: %v", err)))
-			return
-		}
-
-		// Make a new filename - same as old with new extension
-		filename := replaceExt(fheader.Filename, format)
-
-		// Send the picture back
-		w.Header().Set("Content-Disposition", "attachment; filename="+filename)
-		w.Header().Set("Content-Type", http.DetectContentType(convImg))
-		w.Header().Set("Content-Length", strconv.Itoa(len(convImg)))
-		w.Write(convImg)
 
 	case "":
-		fmt.Println("didn't specify format")
-		w.Write([]byte("Didn't specify format :("))
-		return
+		w.Write([]byte(fmt.Sprintf("No file format given :(")))
+
 	default:
-		fmt.Println("conv was", format)
-		w.Write([]byte(fmt.Sprintf("Don't know how to convert to %s :(", format)))
+		w.Write([]byte(fmt.Sprintf("File format %v not supported :(", format)))
+	}
+
+	// Handle 0, 1, 2+ files uploaded
+	switch len(images) {
+	case 0:
+		w.Write([]byte(fmt.Sprintf("No files given :(")))
+		return
+
+	// If only 1 file uploaded, don't need to zip/tar it, just send back the converted image
+	case 1:
+		image, err := images[0].Open()
+		if err != nil {
+			w.Write([]byte(fmt.Sprintf("Couldn't process file: %v", images[0].Filename)))
+			return
+		}
+
+		convImage, err := convert(format, image)
+		if err != nil {
+			w.Write([]byte(fmt.Sprintf("Error converting %v to %v :(", images[0].Filename, format)))
+			return
+		}
+
+		newFilename := replaceExt(images[0].Filename, format)
+
+		err = sendFile(convImage, newFilename, w)
+		if err != nil {
+			w.Write([]byte(fmt.Sprintf("Couldn't send back new file %v :(", newFilename)))
+			return
+		}
+		return
+
+	// Multiple images to handle, need to zip/tar them
+	default:
+		imageBytes := []tarBuffer{}
+		for _, header := range images {
+			image, err := header.Open()
+			if err != nil {
+				// Just skip images that are broken
+				continue
+			}
+			// Might leave this here, just in case
+			// defer image.Close()
+
+			convertedImg, err := convert(format, image)
+			if err != nil {
+				// Just skip images that are broken
+				image.Close()
+				continue
+			}
+			imageBytes = append(imageBytes, tarBuffer{Contents: &convertedImg, Filename: replaceExt(header.Filename, format)})
+			image.Close()
+		}
+
+		// Skipped all the files due to errors
+		if len(imageBytes) < 1 {
+			w.Write([]byte(fmt.Sprintf("No suitable images uploaded :(")))
+			return
+		}
+
+		tar, err := tarFiles(&imageBytes)
+		if err != nil {
+			w.Write([]byte(fmt.Sprintf("Unable to tar requested pictures: %v :(", err)))
+			return
+		}
+
+		err = sendFile(tar, "images.tar", w)
+		if err != nil {
+			w.Write([]byte(fmt.Sprintf("Unable to send back requested pictures: %v :(", err)))
+		}
 		return
 	}
 
@@ -92,4 +148,54 @@ func replaceExt(filename, extension string) string {
 	filename = strings.TrimSuffix(filename, filepath.Ext(filename))
 	filename += ("." + extension)
 	return filename
+}
+
+func sendFile(file []byte, filename string, w http.ResponseWriter) error {
+
+	// Send the picture back
+	w.Header().Set("Content-Disposition", "attachment; filename="+filename)
+	w.Header().Set("Content-Type", http.DetectContentType(file))
+	w.Header().Set("Content-Length", strconv.Itoa(len(file)))
+
+	_, err := w.Write(file)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func tarFiles(images *[]tarBuffer) ([]byte, error) {
+
+	var buf bytes.Buffer
+	tw := tar.NewWriter(&buf)
+
+	for _, img := range *images {
+		hdr := &tar.Header{
+			Name: img.Filename,
+			Mode: 0666,
+			Size: int64(len(*img.Contents)),
+		}
+		err := tw.WriteHeader(hdr)
+		if err != nil {
+			return []byte{}, err
+		}
+
+		_, err = tw.Write(*img.Contents)
+		if err != nil {
+			return []byte{}, err
+		}
+	}
+
+	err := tw.Close()
+	if err != nil {
+		return []byte{}, err
+	}
+
+	return buf.Bytes(), nil
+}
+
+type tarBuffer struct {
+	Contents *[]byte
+	Filename string
 }
